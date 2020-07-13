eaad81f319bdc04dd6cc65e6444d99dbc5c45d7b
Pooja Patel <phpatel.4518@gmail.com>
2020-07-13 01:33:37 -0400

    feat: added registration forms

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,86 +55,118 @@ class UserController extends BaseController
 
     public function register_user(Request $request)
     {
-        // print_r($request->all());exit;
+        //         print_r($request->terms_accept);exit;
 
         $name = $request->username;
         $fname = $request->first_name;
         $lname = $request->last_name;
-        $dob = $request->dob;
         $mail = $request->email;
         $phone = $request->phone_no;
         $type = $request->user_type;
+        $tax_home = isset($request->tax_home) ? $request->tax_home : '0';
 
-        $check = DB::table('users')
-            ->where('client_id', '=', $request->client_id)
-            ->where('email', '=', $request->email)
-            ->get();
-        if (!$check) {
-            return back()
-                ->with('error', 'Email already exists try login')
-                ->with('name', $name)
-                ->with('fname', $fname)
-                ->with('lname', $lname)
-                ->with('mail', $mail)
-                ->with('phone', $phone)
-                ->with('type', $type);
-        }
-        if ($request->user_type == 0) {
-            $validator = Validator::make($request->all(), [
-                //'username' => 'required|unique:users,username',
+        $messages = [
+            'required' => 'Please complete this field',
+            'accepted' => 'Terms and Policy must be agreed',
+            'same' => 'Password must match repeat password',
+            'password1.regex' =>
+                'Password should be at least 7 characters long and should contain at least 1 uppercase, 1 lowercase, 1 number, no special characters allowed',
+            'email.regex' => 'This should be your business email',
+            'website.regex' => 'Please enter valid URL',
+        ];
+
+        if ($request->user_type === "1") {
+            // Owner
+            $rules = [
+                'username' => 'required|unique:users,username',
+                'email' => 'required|email:rfc,dns|unique:users,email',
+                'password1' => 'required|regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{7,}$/i',
+                'password2' => 'required|same:password1',
                 'first_name' => 'required',
                 'last_name' => 'required',
+                'phone_no' => 'required',
                 'dob' => 'required',
-                // 'occupation' => 'required',
-                'password1' => 'required|min:4',
+                'gender' => 'required',
+                'occupation' => 'required',
+                'address' => 'required',
+                'listing_address' => 'required',
+                'terms_accept' => 'accepted',
+            ];
+        } elseif ($request->user_type === "2") {
+            // Travel Agency
+            $rules = [
+                'username' => 'required|unique:users,username',
+                'email' =>
+                    'required|email:rfc,dns|unique:users,email|regex:/^([\w\-.]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)(?!yahoo.co.in)(?!aol.com)(?!abc.com)(?!xyz.com)(?!pqr.com)(?!rediffmail.com)(?!live.com)(?!outlook.com)(?!me.com)(?!msn.com)(?!ymail.com)([\w-]+\.)+[\w-]{2,4})?$/i',
+                'password1' => 'required|regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{7,}$/i',
                 'password2' => 'required|same:password1',
-                'email' => 'required|unique:users,email',
-            ]);
-        }
-        if ($request->user_type == 1) {
-            $validator = Validator::make($request->all(), [
                 'first_name' => 'required',
                 'last_name' => 'required',
+                'phone_no' => 'required',
+                'gender' => 'required',
+                'occupation' => 'required',
+                'work' => 'required',
+                'work_title' => 'required',
+                'website' => 'required|regex:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/',
+                'terms_accept' => 'accepted',
+            ];
+        } elseif ($request->user_type === "3") {
+            // RV Traveler
+            $rules = [
+                'username' => 'required|unique:users,username',
+                'email' => 'required|email:rfc,dns|unique:users,email',
+                'password1' => 'required|regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{7,}$/i',
+                'password2' => 'required|same:password1',
+                'first_name' => 'required',
+                'last_name' => 'required',
+                'phone_no' => 'required',
                 'dob' => 'required',
-                // 'address' => 'required',
-                // 'username' => 'required|unique:users,username',
-                'password1' => 'required|min:4',
+                'gender' => 'required',
+                'occupation' => 'required',
+                'tax_home' => 'required',
+                'address' => 'required',
+                'terms_accept' => 'accepted',
+            ];
+        } else {
+            // Traveler
+            $rules = [
+                'username' => 'required|unique:users,username',
+                'email' => 'required|email:rfc,dns|unique:users,email',
+                'password1' => 'required|regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{7,}$/i',
                 'password2' => 'required|same:password1',
-                'email' => 'required|unique:users,email',
-            ]);
+                'first_name' => 'required',
+                'last_name' => 'required',
+                'phone_no' => 'required',
+                'dob' => 'required',
+                'gender' => 'required',
+                'tax_home' => 'required',
+                'address' => 'required',
+                'terms_accept' => 'accepted',
+            ];
         }
-        if ($request->user_type == 2) {
-            $validator = Validator::make($request->all(), [
-                //'username' => 'required|unique:users,username',
-                'password1' => 'required|min:4',
-                'name_of_agency' => 'required',
-                // 'address' => 'required',
-                'password2' => 'required|same:password1',
-                'email' => 'required|unique:users,email',
-            ]);
-        }
+
+        $validator = Validator::make($request->all(), $rules, $messages);
 
         if ($validator->fails()) {
-            $error_messages = implode(',', $validator->messages()->all());
             return back()
-                ->with('error', $error_messages)
-                ->with('name', $name)
+                ->with('username', $name)
                 ->with('fname', $fname)
                 ->with('lname', $lname)
                 ->with('mail', $mail)
                 ->with('phone', $phone)
-                ->with('name', $type);
-        }
-
-        if (!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{7,}$/', $request->password1)) {
-            return back()
-                ->with('error', 'Password atleast 7 characters and 1 upper 1 lower and 1 number')
-                ->with('name', $name)
-                ->with('fname', $fname)
-                ->with('lname', $lname)
-                ->with('mail', $mail)
-                ->with('phone', $phone)
-                ->with('type', $type);
+                ->with('type', $type)
+                ->with('selectedTab', 'tab2')
+                ->with('terms_accept', $request->terms_accept)
+                ->with('dob', $request->dob)
+                ->with('gender', $request->gender)
+                ->with('languages_known', $request->languages_known)
+                ->with('tax_home', $request->tax_home)
+                ->with('address', $request->address)
+                ->with('listing_address', $request->listing_address)
+                ->with('work', $request->work)
+                ->with('work_title', $request->work_title)
+                ->with('website', $request->website)
+                ->withErrors($validator);
         }
 
         $password = $this->encrypt_password($request->password1);
@@ -151,20 +183,23 @@ class UserController extends BaseController
         $token = $this->generate_random_string();
         $mobile = $request->phone_no;
         $insert = DB::table('users')->insert([
+            'client_id' => $request->client_id,
+            'role_id' => $role_id,
             'username' => $request->username,
+            'email' => $request->email,
+            'password' => $password,
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
-            'client_id' => $request->client_id,
-            'email' => $request->email,
             'phone' => $request->phone_no,
             'date_of_birth' => $request->dob,
+            'gender' => $request->gender,
+            'languages_known' => $request->languages_known,
+            'occupation' => $request->occupation,
             'occupation_desc' => $request->occupation_desc,
-            'password' => $password,
-            'role_id' => $role_id,
+            'name_of_agency' => $request->name_of_agency,
+            'tax_home' => $tax_home,
             'status' => 1,
             'address' => $request->address,
-            'occupation' => $request->occupation,
-            'name_of_agency' => $request->name_of_agency,
             'auth_token' => $token,
         ]);
         $d = DB::table('users')
@@ -174,7 +209,7 @@ class UserController extends BaseController
         $request->session()->put('phone', $d->phone);
         $request->session()->put('user_id', $d->id);
         $request->session()->put('role_id', $d->role_id);
-        $request->session()->put('username', $d->first_name . " " . $d->last_name);
+        $request->session()->put('username', $d->username);
 
         $verify_url = BASE_URL . 'verify/' . $d->id . '/' . $token;
 
--- resources/views/includes/styles.blade.php
+++ resources/views/includes/styles.blade.php
@@ -1,81 +1,81 @@
 <link rel="shortcut icon" href="{{URL::asset('favicon.ico')}}" type="image/x-icon">
 
-  <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
 
-  <link rel="stylesheet" href="{{ URL::asset('css/colors/main.css') }}" id="colors">
-  <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-datepicker.min.css') }}">
-  <script src="{{ URL::asset('lib/jquery.min.js') }}"></script>
-  <script src="{{ URL::asset('lib/bootstrap.min.js') }}"></script>
-  <script src="{{ URL::asset('lib/moment.min.js') }}"></script>
+<link rel="stylesheet" href="{{ URL::asset('css/colors/main.css') }}" id="colors">
+<link rel="stylesheet" href="{{ URL::asset('css/bootstrap-datepicker.min.css') }}">
+<script src="{{ URL::asset('lib/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('lib/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('lib/moment.min.js') }}"></script>
 <script src="{{ URL::asset('lib/jquery-ui.custom.min.js') }}"></script>
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('calendar/calendar1.css')}}" />
 <script src="{{ URL::asset('calendar/calendar.js') }}"></script>
 <script src="{{ URL::asset('lib/bootbox.min.js') }}"></script>
 
- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/locales/bootstrap-datepicker.tr.min.js"></script>
- <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-timepicker.min.css') }}">
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-timepicker/0.5.2/js/bootstrap-timepicker.min.js"></script>
+<link rel="stylesheet" href="{{ URL::asset('css/bootstrap-timepicker.min.css') }}">
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-timepicker/0.5.2/js/bootstrap-timepicker.min.js"></script>
 
-  <link rel="stylesheet" href="{{URL::asset('css/caleran.min.css')}}">
-  <!-- <script type="text/javascript" src="{{URL::asset('js/moment.min.js')}}"></script> -->
+<link rel="stylesheet" href="{{URL::asset('css/caleran.min.css')}}">
+<!-- <script type="text/javascript" src="{{URL::asset('js/moment.min.js')}}"></script> -->
 
-  <script type="text/javascript" src="{{URL::asset('js/caleran.min.js')}}"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+<script type="text/javascript" src="{{URL::asset('js/caleran.min.js')}}"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
 
-  {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
-  <style type="text/css">
+{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
+<style type="text/css">
     #snackbar {
-      visibility: hidden; /* Hidden by default. Visible on click */
-      min-width: 250px; /* Set a default minimum width */
-      margin-left: -125px; /* Divide value of min-width by 2 */
-      background-color: #333; /* Black background color */
-      color: #fff; /* White text color */
-      text-align: center; /* Centered text */
-      border-radius: 2px; /* Rounded borders */
-      padding: 16px; /* Padding */
-      position: fixed; /* Sit on top of the screen */
-      z-index: 1; /* Add a z-index if needed */
-      left: 50%; /* Center the snackbar */
-      bottom: 30px; /* 30px from the bottom */
+        visibility: hidden; /* Hidden by default. Visible on click */
+        min-width: 250px; /* Set a default minimum width */
+        margin-left: -125px; /* Divide value of min-width by 2 */
+        background-color: #333; /* Black background color */
+        color: #fff; /* White text color */
+        text-align: center; /* Centered text */
+        border-radius: 2px; /* Rounded borders */
+        padding: 16px; /* Padding */
+        position: fixed; /* Sit on top of the screen */
+        z-index: 1; /* Add a z-index if needed */
+        left: 50%; /* Center the snackbar */
+        bottom: 30px; /* 30px from the bottom */
     }
 
     /* Show the snackbar when clicking on a button (class added with JavaScript) */
     #snackbar.show {
-      visibility: visible; /* Show the snackbar */
+        visibility: visible; /* Show the snackbar */
 
-      /* Add animation: Take 0.5 seconds to fade in and out the snackbar.
-      However, delay the fade out process for 2.5 seconds */
-      -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-      animation: fadein 0.5s, fadeout 0.5s 2.5s;
+        /* Add animation: Take 0.5 seconds to fade in and out the snackbar.
+        However, delay the fade out process for 2.5 seconds */
+        -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+        animation: fadein 0.5s, fadeout 0.5s 2.5s;
     }
 
     /* Animations to fade the snackbar in and out */
     @-webkit-keyframes fadein {
-      from {bottom: 0; opacity: 0;}
-      to {bottom: 30px; opacity: 1;}
+        from {bottom: 0; opacity: 0;}
+        to {bottom: 30px; opacity: 1;}
     }
 
     @keyframes fadein {
-      from {bottom: 0; opacity: 0;}
-      to {bottom: 30px; opacity: 1;}
+        from {bottom: 0; opacity: 0;}
+        to {bottom: 30px; opacity: 1;}
     }
 
     @-webkit-keyframes fadeout {
-      from {bottom: 30px; opacity: 1;}
-      to {bottom: 0; opacity: 0;}
+        from {bottom: 30px; opacity: 1;}
+        to {bottom: 0; opacity: 0;}
     }
 
     @keyframes fadeout {
-      from {bottom: 30px; opacity: 1;}
-      to {bottom: 0; opacity: 0;}
+        from {bottom: 30px; opacity: 1;}
+        to {bottom: 0; opacity: 0;}
     }
 
     a:focus, a:hover {
-      color: #e78016;
-      text-decoration: underline;
+        color: #e78016;
+        text-decoration: underline;
     }
 
     .alert-success {
@@ -83,4 +83,4 @@
         background-color: #e78016 !important;
         border-color: #ffffff;
     }
-  </style>
+</style>

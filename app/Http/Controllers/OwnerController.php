5f6ab6d0f6402ce40acdd240401e3c7e9b7123d7
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-07-14 20:59:23 +0530

    feat: added owner profile page

---
+++ app/Http/Controllers/OwnerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Models\Users;
+use App\Http\Controllers\Validator;
+use DB;
+use \stdClass;
+
+class OwnerController extends BaseController
+{
+    public function owner_profile(Request $request)
+    {
+        $user_id = $request->session()->get('user_id');
+        if (!$user_id) {
+            return redirect('/login')->with('error', 'Session timeout login again');
+        }
+        $client_id = $this->get_client_id();
+        $user_detail = DB::table('users')
+            ->where('client_id', '=', $client_id)
+            ->where('id', '=', $user_id)
+            ->first();
+
+        foreach ($user_detail as $key => $value) {
+            if ($value == '0') {
+                $user_detail->$key = "";
+            }
+        }
+
+        $agency = DB::table('agency')->get();
+        $occupation = DB::table('occupation')->get();
+
+        $country_codes = DB::table('country_code')
+            ->where('client_id', '=', $client_id)
+            ->get();
+
+        return view('owner.profile', [
+            'user_detail' => $user_detail,
+            'country_codes' => $country_codes,
+            'agency' => $agency,
+            'occupation' => $occupation,
+        ]);
+    }
+}
---
+++ resources/views/owner/menu.blade.php
@@ -0,0 +1,213 @@
+<style type="text/css">
+    .fa-bell {
+        color: #0983b8;
+        display: none;
+    }
+</style>
+@if(Session::get('role_id') == 1 || Session::get('role_id') == 3)
+<ul class="my-account-nav">
+
+    <li class="sub-nav-title">Manage Account</li>
+
+    <li>
+        <a href="{{url('/')}}/owner/profile" {{{ (Request::is('owner/profile') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-user"></i> My Profile
+        </a>
+    </li>
+    <li>
+        <a href="{{url('/')}}/owner/verify-account" {{{ (Request::is('owner/verify-account') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-user"></i>Verify Account
+        </a>
+    </li>
+
+</ul> 
+
+<ul class="my-account-nav">
+
+    <li class="sub-nav-title">Manage Listings</li>
+
+    <li>
+        <a href="{{url('/')}}/owner/my-properties" {{{ (Request::is('owner/my-properties') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-home"></i> My Properties
+        </a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/owner/add-property" {{{ (Request::is('owner/add-property') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-plus"></i> Add New Property
+        </a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/owner/favorites" {{{ (Request::is('owner/favorites') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-star"></i> Favorites 
+        </a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/owner/my-bookings" {{{ (Request::is('owner/my-bookings') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-basket"></i> My Bookings 
+        </a><i class="fa fa-bell" id="owner_booking"></i>
+    </li>
+
+   {{--  <li>
+        <a href="{{url('/')}}/owner/reservations" {{{ (Request::is('owner/reservations') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-credit-card"></i> My Trips
+        </a>
+    </li> --}}
+
+    <li>
+        <a href="{{url('/')}}/owner/calender" {{{ (Request::is('owner/calender') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-credit-card"></i> Calender
+        </a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/owner/inbox" {{{ (Request::is('owner/inbox') ? 'class=current' : '') }}}>
+           <i class="fa fa-inbox"></i> My Inbox
+        </a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/owner/transcation-history" {{{ (Request::is('owner/transcation-history') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-note"></i> Transcation History
+        </a>
+    </li>
+ <li>
+        <a href="{{url('/')}}/owner/special_price" {{{ (Request::is('/owner/special_price') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-star"></i>  Special Pricing 
+        </a>
+    </li>
+
+</ul>
+
+
+
+<ul class="my-account-nav">
+
+    {{-- <li>
+        <a href="{{url('/')}}/owner/payment-method" {{{ (Request::is('owner/payment-method') ? 'class=current' : '') }}}>
+           <i class="fa fa-plus"></i> Payment Method
+        </a>
+    </li> --}}
+    <li>
+        <a href="{{url('/')}}/delete_account" {{{ (Request::is('delete_account') ? 'class=current' : '') }}}>
+           <i class="fa fa-trash"></i> Delete My Account
+        </a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/owner/change-password" {{{ (Request::is('owner/change-password') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-lock"></i> Change Password
+        </a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/logout" {{{ (Request::is('home') ? 'class=current' : '') }}}>
+            <i class="sl sl-icon-power"></i> Log Out
+        </a>
+    </li>
+
+</ul>
+@endif
+
+@if(Session::get('role_id') == 0 ) {{-- 1- owner 0 - traveller-- 2 -- Travel Agency --}}
+<ul class="my-account-nav">
+    <li class="sub-nav-title">Manage Account</li>
+
+    <li><a href="{{url('/')}}/traveler/profile" {{{ (Request::is('traveler/profile') ? 'class=current' : '') }}}>
+        <i class="sl sl-icon-user"></i> My Profile</a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/traveler/verify-account" {{{ (Request::is('traveler/verify-account') ? 'class=current' : '') }}}>
+            <i class="sl sl-icon-user"></i>Verify Account
+        </a>
+    </li>
+
+    <li><a href="{{url('/')}}/traveler/favorites" {{{ (Request::is('traveler/favorites') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-star"></i> Favorites </a></li>
+
+    <li><a href="{{url('/')}}/traveler/my-reservations" {{{ (Request::is('traveler/my-reservations') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-credit-card"></i> My Trips</a><i class="fa fa-bell" id="traveler_trips"></i></li>
+
+    <li><a href="{{url('/')}}/traveler/inbox" {{{ (Request::is('traveler/inbox') ? 'class=current' : '') }}}>
+           <i class="fa fa-inbox"></i> Inbox</a></li>
+<li>
+        <a href="{{url('/')}}/owner/delete_account" {{{ (Request::is('delete_account') ? 'class=current' : '') }}}>
+           <i class="fa fa-trash"></i> Delete My Account
+        </a>
+    </li>
+<li>
+        <a href="{{url('/')}}/owner/change-password" {{{ (Request::is('owner/change-password') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-lock"></i> Change Password
+        </a>
+    </li>
+    <li><a href="{{url('/')}}/logout" onclick="signOut();" {{{ (Request::is('logout') ? 'class=current' : '') }}}>
+            <i class="sl sl-icon-power"></i> Log Out</a></li>
+</ul>
+@endif
+
+
+@if(Session::get('role_id') == 2 ) {{-- 1- owner 0 - traveller-- 2 -- Travel Agency --}}
+<ul class="my-account-nav">
+    <li class="sub-nav-title">Manage Account</li>
+
+    <li><a href="{{url('/')}}/traveler/profile" {{{ (Request::is('traveler/profile') ? 'class=current' : '') }}}>
+        <i class="sl sl-icon-user"></i> My Profile</a>
+    </li>
+
+    <li>
+        <a href="{{url('/')}}/agency/verify-account" {{{ (Request::is('agency/verify-account') ? 'class=current' : '') }}}>
+            <i class="sl sl-icon-user"></i>Verify Account
+        </a>
+    </li>
+
+    <li><a href="{{url('/')}}/traveler/favorites" {{{ (Request::is('traveler/favorites') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-star"></i> Favorites </a></li>
+
+    <li><a href="{{url('/')}}/traveler/my-reservations" {{{ (Request::is('traveler/my-reservations') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-credit-card"></i> My Trips</a><i class="fa fa-bell" id="agency_trips"></i></li>
+
+    <li><a href="{{url('/')}}/traveler/inbox" {{{ (Request::is('traveler/inbox') ? 'class=current' : '') }}}>
+           <i class="fa fa-inbox"></i> Inbox</a></li>
+<li>
+        <a href="{{url('/')}}/delete_account" {{{ (Request::is('delete_account') ? 'class=current' : '') }}}>
+           <i class="fa fa-trash"></i> Delete My Account
+        </a>
+    </li>
+<li>
+        <a href="{{url('/')}}/owner/change-password" {{{ (Request::is('owner/change-password') ? 'class=current' : '') }}}>
+           <i class="sl sl-icon-lock"></i> Change Password
+        </a>
+    </li>
+    <li><a href="{{url('/')}}/logout" onclick="signOut();" {{{ (Request::is('logout') ? 'class=current' : '') }}}>
+            <i class="sl sl-icon-power"></i> Log Out</a></li>
+</ul>
+@endif
+<script type="text/javascript">
+    $(document).ready(function(){
+        var user_id = {{Session::get('user_id')}};
+        $("#owner_booking").hide();
+        $("#traveler_trips").hide();
+        $("#agency_trips").hide();
+        var url = "{{url('/')}}/get-user-notifications?user_id="+user_id;
+        console.log(url);
+        $.ajax({
+            "type": "get",
+            "url" : url,
+            success: function(data) {
+              console.log(data.trip_count);
+              if(data.status=="SUCCESS"){
+                 if(data.booking_count > 0){
+                    $("#owner_booking").show();
+                 }
+                 if(data.trip_count > 0){
+                    $("#traveler_trips").show();
+                    $("#agency_trips").show();
+                 }
+              }
+            }
+        });
+    });      
+</script>

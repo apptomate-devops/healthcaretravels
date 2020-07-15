1048adeb8b4b94d14483f4a46788802f43639725
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-07-15 12:19:20 +0530

    feat: added custom logger

---
+++ app/Services/Logger.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+use Illuminate\Support\Facades\Log;
+
+class Logger
+{
+    public static function getFilePrefix()
+    {
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3);
+        $fileCaller = $trace[1];
+        $functionCaller = $trace[2];
+        $path_parts = pathinfo($fileCaller['file']);
+        $fileName = $path_parts['filename'];
+        $functionName = $functionCaller['function'];
+        return $fileName . "(" . $functionName . "): ";
+    }
+    public static function info($message)
+    {
+        Log::info(Logger::getFilePrefix() . $message);
+    }
+    public static function error($message)
+    {
+        Log::error(Logger::getFilePrefix() . $message);
+    }
+}

d41b36955eb81111a08e26db65ed8477db794f4d
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-07-16 22:01:02 +0530

    fix: seeder to not run multiple times

--- database/seeds/UserRoleTableSeeder.php
+++ database/seeds/UserRoleTableSeeder.php
@@ -11,11 +11,16 @@ class UserRoleTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('user_role')->insert([
-            'role' => 'RV TRAVELLER',
-            'client_id' => 15465793,
-            'status' => 1,
-            'updated_at' => date("Y-m-d H:i:s"),
-        ]);
+        \App\Models\UserRole::updateOrCreate(
+            [
+                'role' => 'RV TRAVELLER',
+            ],
+            [
+                'role' => 'RV TRAVELLER',
+                'client_id' => 15465793,
+                'status' => 1,
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+        );
     }
 }

e65ecee24db4235fd2fb61de5a42293819125c7c
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-07-18 20:34:59 +0530

    fix: cicd related fixes

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -17,7 +17,7 @@
 @task('clone_repository')
     echo 'Cloning repository'
     [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
-    git clone --depth 1 --branch feat/cicd {{ $repository }} {{ $new_release_dir }}
+    git clone --depth 1 --branch develop {{ $repository }} {{ $new_release_dir }}
     cd {{ $new_release_dir }}
     git reset --hard {{ $commit }}
 @endtask

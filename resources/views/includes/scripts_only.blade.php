99da6b9ae23a54378d2856daf1fc120038b0af91
Pooja Patel <phpatel.4518@gmail.com>
2020-07-24 12:48:06 -0400

    fix: removed multiple declaration of google map script

--- resources/views/includes/scripts_only.blade.php
+++ resources/views/includes/scripts_only.blade.php
@@ -28,8 +28,8 @@
 
 </script>
 
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyByI8gik-nps54DdqY81oqS1GCFJK8mko4&sensor=false&libraries=places&callback=initMap" async defer></script>
+
+{{--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyByI8gik-nps54DdqY81oqS1GCFJK8mko4&libraries=places&callback=initMap" async defer></script>--}}
 
 <script src="https://apis.google.com/js/platform.js?onload=onLoad" async defer></script>
 

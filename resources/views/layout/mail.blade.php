e2d0421b49943d71b64b99bd0374ed789fd8d382
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-07-31 21:51:16 +0530

    refactor: image center align in email

--- resources/views/layout/mail.blade.php
+++ resources/views/layout/mail.blade.php
@@ -3,15 +3,15 @@
 <body>
 <div style="width: auto;border: 10px solid #d8750f;padding: 20px;margin: 10px;">
     <div class="container">
-        <div class="navbar-header">
-            <div style="text-align: center;">
-                <a href="" title="" style="margin-top:0px">
-                    <img src="{{BASE_URL}}uploads/keepers_logo.png" height="100px" width="100px" class="img-responsive logo-new">
-                </a>
-            </div>
-            <span style="float:right; text-align:right;"></span>
-            <div style="clear:both;"></div>
-        </div>
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td align="center">
+                    <a href="{{BASE_URL}}" style="margin-top:0px">
+                        <img title="Logo" alt="Logo" src="{{BASE_URL}}uploads/keepers_logo.png" height="100px" width="100px" class="img-responsive logo-new">
+                    </a>
+                </td>
+            </tr>
+        </table>
         <hr width="100%"/>
         <div class="mail-container"style="margin: 1em 0; display: inline-block; font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif; padding: 0; display: inline-block;">
             @yield('content')

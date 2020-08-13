8b6a6028d6ebd0ef1eb2c05d4eb0517ef7411bc2
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-08-13 14:16:01 +0530

    feat: fix all over image urls issue

---
+++ database/seeds/HandlePublicURLSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class HandlePublicURLSeeder extends Seeder
+{
+    public function updatePathFields($model, $column, $subject, $replace = "")
+    {
+        $rows = $model
+            ::query()
+            ->where($column, 'LIKE', "%" . $subject . "%")
+            ->get();
+        Log::info($column . ' that needs to be replaced: count: ' . count($rows));
+        echo $column . ' that needs to be replaced: count: ' . count($rows) . PHP_EOL;
+        foreach ($rows as $row) {
+            $new_url = str_replace($subject, $replace, $row->$column);
+            $model::where('id', $row->id)->update([$column => $new_url]);
+        }
+    }
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $cs = 'public/'; // Common Subject
+        $this->updatePathFields(\App\Models\AmenitiesList::class, 'icon_url', $cs, '');
+        $this->updatePathFields(\App\Models\Documents::class, 'document_url', $cs, '');
+        $this->updatePathFields(\App\Models\HomeImages::class, 'image_url', $cs, '');
+        $this->updatePathFields(\App\Models\HomeListing::class, 'image_url', $cs, '');
+        $this->updatePathFields(\App\Models\Propertyamenties::class, 'amenties_icon', $cs, '');
+        $this->updatePathFields(\App\Models\Propertyimage::class, 'image_url', $cs, '');
+        $this->updatePathFields(\App\Models\Users::class, 'profile_image', $cs, '');
+        $this->updatePathFields(
+            \App\Models\Settings::class,
+            'value',
+            'healthcaretravels.com/public/',
+            'healthcaretravels.com/',
+        );
+
+        // Note: Ignoring test_table as it is not being used in the code
+    }
+}

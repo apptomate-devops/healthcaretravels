185f9915c4f87ba6a73ad5c260251ab4d4f15461
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-08-27 11:23:12 +0530

    chore: changed google analytics to new site code

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -9,13 +9,13 @@
 
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123742839-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117530617-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-123742839-1');
+  gtag('config', 'UA-117530617-1');
 </script>
 <!-- CSS
 ================================================== -->

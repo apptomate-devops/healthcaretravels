a20edbf863c5d9c566bb87ab676ecef0f467b9e2
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-09-02 08:29:59 +0530

    chore: two factor disabled by default

--- database/migrations/2020_09_02_025046_change_two_factor_default_value_of_users.php
+++ database/migrations/2020_09_02_025046_change_two_factor_default_value_of_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddEnableTwoFectorAuthToUsers extends Migration
+class ChangeTwoFactorDefaultValueOfUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,10 @@ class AddEnableTwoFectorAuthToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('enable_two_factor_auth')->default(1);
+            $table
+                ->string('enable_two_factor_auth')
+                ->default(0)
+                ->change();
         });
     }
 
@@ -26,7 +29,10 @@ class AddEnableTwoFectorAuthToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['enable_two_factor_auth']);
+            $table
+                ->string('enable_two_factor_auth')
+                ->default(1)
+                ->change();
         });
     }
 }

d36e065b8d52dd4ab98a7cd7f837cda0699d8334
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-09-09 15:54:26 +0530

    feat: creating funding source for user using dwolla js

--- resources/views/payment_test.blade.php
+++ resources/views/payment_test.blade.php
@@ -2,40 +2,14 @@
 
 @section('main_content')
 <script src="https://cdn.dwolla.com/1/dwolla.js"></script>
-<script>
-    dwolla.configure('{{DWOLLA_ENV}}');
-    function fundingSourcecallback(err, res) {
-        var $div = $("<div />");
-        var logValue = {
-            error: err,
-            response: res,
-        };
-        $div.text(JSON.stringify(logValue));
-        console.log(logValue);
-        $('#logs').append($div);
-    }
-    $('#create-funding-source').on('submit', function () {
-        var token = '{{$token}}';
-        var bankInfo = {
-            routingNumber: $('#routingNumber').val(),
-            accountNumber: $('#accountNumber').val(),
-            type: $('#type').val(),
-            name: $('#name').val(),
-        };
-        dwolla.fundingSources.create(token, bankInfo, fundingSourcecallback);
-        return false;
-    });
-</script>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            {{$token}}
             <form id="create-funding-source">
-                <p class="form-row form-row-wide">
-                    <label for="password">Routing number:
-                        <input class="input-text" type="text" id="routingNumber" placeholder="273222226" required />
-                    </label>
-                </p>
+                <div>
+                    <label for="password">Routing number</label>
+                    <input type="text" id="routingNumber" placeholder="273222226" required />
+                </div>
                 <div>
                     <label>Account number</label>
                     <input type="text" id="accountNumber" placeholder="Account number" />
@@ -51,12 +25,53 @@
                     </select>
                 </div>
                 <div>
-                    <input type="submit" value="Add Bank" />
+                    <input type="submit" value="Add Bank" class="btn btn-primary" />
                 </div>
             </form>
             <div id="logs"></div>
         </div>
     </div>
 </div>
+<script>
+    dwolla.configure('{{DWOLLA_ENV}}');
+    function fundingSourcecallback(err, res) {
+        var $div = $("<div />");
+        var logValue = {
+            error: err,
+            response: res,
+        };
+        if (err) {
+            var errors = err._embedded.errors;
+            errors.forEach(function (errDetail) {
+                var errorField = errDetail.path.substring(1);
+                var fieldNode = $('#' + errorField);
+                fieldNode.addClass('form-error');
+                $('<p class="txt-red mb-5 form-error-message">' + errDetail.message + '</p>').insertAfter(fieldNode);
+            });
+            return false;
+        }
+        // TODO: send me to server to update me for user
+        var fundingSource = res._links['funding-source'].href;
+        alert(fundingSource);
+        $div.text(JSON.stringify(logValue));
+        console.log(logValue);
+        $('#logs').append($div);
+    }
+    $('#create-funding-source').on('submit', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(".form-error").removeClass('form-error');
+        $(".form-error-message").hide();
+        var token = '{{$token}}';
+        var bankInfo = {
+            routingNumber: $('#routingNumber').val(),
+            accountNumber: $('#accountNumber').val(),
+            type: $('#type').val(),
+            name: $('#name').val(),
+        };
+        dwolla.fundingSources.create(token, bankInfo, fundingSourcecallback);
+        return false;
+    });
+</script>
 @endsection
 

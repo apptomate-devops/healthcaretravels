288051f209ac51aae0a8cb084078a2e416adb977
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-09-17 21:58:27 +0530

    feat: added booking relation to booking payments

--- app/Models/BookingPayments.php
+++ app/Models/BookingPayments.php
@@ -8,4 +8,12 @@ class BookingPayments extends Model
 {
     protected $table = 'booking_payments';
     protected $primaryKey = 'id';
+
+    /**
+     * Get the Booking that owns the payments.
+     */
+    public function booking()
+    {
+        return $this->belongsTo('App\Models\PropertyBooking', 'booking_id', 'booking_id');
+    }
 }

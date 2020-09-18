ffe6129566e5207db9c0192d04c159db54ed93cf
Parth Vakharia <pvakharia007@gmail.com>
2020-09-18 17:07:49 +0530

    fix: unwanted imports removed

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use App\Jobs\ProcessEmail;
 
 class SendEmailController extends BaseController

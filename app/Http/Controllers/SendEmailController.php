afa13bdc227968161eb2d8034463f4b2ffa7c093
Parth Vakharia <pvakharia007@gmail.com>
2020-09-18 10:07:11 +0530

    fix: job for sending scheduled emails

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Jobs\ProcessEmail;
 
 class SendEmailController extends BaseController
@@ -10,8 +11,26 @@ class SendEmailController extends BaseController
     //
     public function createJobForEmail()
     {
-        ProcessEmail::dispatch()->delay(now()->addMinutes(10));
-        ProcessEmail::dispatch()->delay(now()->addMinutes(10));
-        ProcessEmail::dispatch()->delay(now()->addMinutes(10));
+        error_log('email added');
+        $data1 = [
+            'name' => 'htrap007',
+            'travelerName' => 'helloworld',
+            'propertyName' => 'foo',
+        ];
+        $data2 = [
+            'name' => '007htrap',
+            'travelerName' => 'helloworld',
+            'propertyName' => 'foo',
+        ];
+        ProcessEmail::dispatch(
+            "pvakharia007@gmail.com",
+            "mail.owner-24hr-before-checkin",
+            "First subject",
+            $data1,
+            APP_BASE_NAME,
+        )->delay(now());
+        // ProcessEmail::dispatch("007pvakharia@gmail.com", "mail.owner-24hr-before-checkin", "Second subject", $data2,APP_BASE_NAME)->delay(now());
+        // ProcessEmail::dispatch()->delay(now());
+        // ProcessEmail::dispatch()->delay(now()->addMinutes(10));
     }
 }
--- app/Jobs/ProcessEmail.php
+++ app/Jobs/ProcessEmail.php
@@ -2,23 +2,35 @@
 
 namespace App\Jobs;
 
+use App\Http\Controllers\ConstantsController;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
 
 class ProcessEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    protected $email;
+    protected $to;
+    protected $view;
+    protected $data;
+    protected $subject;
+    protected $app_base_name;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($to, $view, $subject, $data, $app_base_name)
     {
+        $this->to = $to;
+        $this->view = $view;
+        $this->subject = $subject;
+        $this->data = $data;
+        $this->app_base_name = $app_base_name;
     }
 
     /**
@@ -28,6 +40,26 @@ class ProcessEmail implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $toEmail = $this->to;
+        $subject = $this->subject;
+        $app_base_name = $this->app_base_name;
+
+        error_log('trying to send ' . $toEmail);
+        try {
+            Mail::send($this->view, $this->data, function ($message) use ($toEmail, $subject, $app_base_name) {
+                $message->from('gotocva@gmail.com', 'Mail from ' . $app_base_name);
+                $message->to($toEmail);
+                $message->subject($subject);
+            });
+            error_log('email send to ' . $toEmail);
+        } catch (Exception $e) {
+            error_log($e);
+        }
+    }
+
+    public function failed()
+    {
+        $toEmail = $this->to;
+        error_log('job is failing ' . $toEmail);
     }
 }

3b36a219d2bf40a2d535187f287d5124e54c546f
Parth Vakharia <pvakharia007@gmail.com>
2020-09-18 17:07:17 +0530

    fix: logs removed

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -11,7 +11,6 @@ class SendEmailController extends BaseController
     //
     public function createJobForEmail()
     {
-        error_log('email added');
         $data1 = [
             'name' => 'htrap007',
             'travelerName' => 'helloworld',
@@ -19,16 +18,8 @@ class SendEmailController extends BaseController
             'BASE_URL' => BASE_URL,
             'APP_BASE_NAME' => APP_BASE_NAME,
         ];
-        $data2 = [
-            'name' => 'htrap007',
-            'travelerName' => 'helloworld',
-            'propertyName' => 'foo',
-        ];
         ProcessEmail::dispatch("test@gmail.com", "mail.owner-24hr-before-checkin", "First subject", $data1)->delay(
-            now()->addMinutes(0.5),
+            now()->addSeconds(30),
         );
-        // ProcessEmail::dispatch("007pvakharia@gmail.com", "mail.owner-24hr-before-checkin", "Second subject", $data2,APP_BASE_NAME)->delay(now());
-        // ProcessEmail::dispatch()->delay(now());
-        // ProcessEmail::dispatch()->delay(now()->addMinutes(10));
     }
 }

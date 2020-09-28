05a0b61905122797606b450f12b40da33cbef0e7
Parth Vakharia <pvakharia007@gmail.com>
2020-09-28 19:12:27 +0530

    feat: admin cancel booking email template

---
+++ resources/views/mail/admin-cancel-booking.blade.php
@@ -0,0 +1,9 @@
+<div>
+    You booking at {{propertyName}} for {{checkin}} - {{checkout}} has been cancelled. Please reach out to support for more information.
+</div>
+<div style="padding-top: 5px;">
+    Traveler: {{travellerName}}
+</div>
+<div style="padding-top: 5px;">
+    Property Owner: {{ownerName}}
+</div>

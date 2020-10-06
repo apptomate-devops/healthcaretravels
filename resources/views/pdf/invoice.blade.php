72651ae474fbe82f0f257183e18f86f22a921975
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-10-06 21:14:50 +0530

    fix: security deposit date

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -79,7 +79,7 @@
     @if($data->is_owner == 0)
         {{--                    TODO: Add security deposit details here when handled--}}
         <tr style="height: 54px;">
-            <td>{{date('m/d/Y',strtotime($data->start_date))}}</td>
+            <td>{{\Carbon\Carbon::parse($data->end_date)->addHours(72)->format('m/d/Y')}}</td>
             <td>Security Deposit</td>
             <td>${{$data->security_deposit}}</td>
             <td>

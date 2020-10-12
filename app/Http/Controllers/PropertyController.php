4ce7d657603f3b373e0ab3db8cfd320ffd00729b
Pooja Patel <phpatel.4518@gmail.com>
2020-10-12 14:38:26 -0400

    fix: bed count on property page

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -1375,6 +1375,7 @@ class PropertyController extends BaseController
             }
 
             $properties->bed_rooms = $bed_list;
+            $properties->bed_count = $bed_list->sum('count');
 
             $amenties = DB::table('property_amenties')
                 ->where('property_amenties.client_id', '=', CLIENT_ID)

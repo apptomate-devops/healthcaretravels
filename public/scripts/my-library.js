10bf2c3e6fb1a88939797b4c09b828af863e4667
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2020-10-22 17:22:35 +0530

    feat: allowed cleaning fee and security deposit to be 0

--- public/scripts/my-library.js
+++ public/scripts/my-library.js
@@ -1,12 +1,19 @@
 $('#button, #button1').click(function (e) {
     $(".validate, .validate1").each(function () {
         var value = $(this).val();
-        if (value && value.trim() && value != 0) {
+        var fieldId = $(this).attr('id');
+        var allowedZero = ['cleaning_fee', 'security_deposit'];
+        var isAllowedZero = allowedZero.indexOf(fieldId) !== -1;
+        var isValid = value && value.trim() && value != 0;
+        if (isAllowedZero && value.trim() == 0) {
+            isValid = true;
+        }
+        if (isValid) {
             $(this).css({
                 "border": "2px solid green",
                 "background": ""
             });
-            if($(this).is('select')) {
+            if ($(this).is('select')) {
                 $(this).next('.chosen-container').css({
                     "border": "2px solid green",
                     "background": ""
@@ -19,14 +26,14 @@ $('#button, #button1').click(function (e) {
                 "border": "1px solid red",
                 "background": ""
             });
-            if($(this).is('select')) {
+            if ($(this).is('select')) {
                 $(this).next('.chosen-container').css({
                     "border": "1px solid red",
                     "background": ""
                 })
             }
             try {
-                $(window).scrollTop($(this).offset().top-100);
+                $(window).scrollTop($(this).offset().top - 100);
             } catch (e) {
                 console.log('Error while validate:', e);
             }

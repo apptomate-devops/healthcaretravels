7d97ad6fcd359472db8ff8754c28ab79a231bacb
Pooja Patel <phpatel.4518@gmail.com>
2020-11-12 13:25:00 -0500

    fix: added new amenities

---
+++ database/seeds/AmenityListSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AmenityListSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\AmenitiesList::updateOrCreate(
+            [
+                'amenities_name' => 'Roku',
+            ],
+            [
+                'amenities_name' => 'Roku',
+                'icon_url' => 'amenities/roku.png',
+                'client_id' => 15465793,
+                'status' => 1,
+            ],
+        );
+        \App\Models\AmenitiesList::updateOrCreate(
+            [
+                'amenities_name' => 'Iron/Ironing Board',
+            ],
+            [
+                'amenities_name' => 'Iron/Ironing Board',
+                'icon_url' => 'amenities/iron.png',
+                'client_id' => 15465793,
+                'status' => 1,
+            ],
+        );
+        \App\Models\AmenitiesList::updateOrCreate(
+            [
+                'amenities_name' => 'Smart TV',
+            ],
+            [
+                'amenities_name' => 'Smart TV',
+                'icon_url' => 'amenities/smarttv.png',
+                'client_id' => 15465793,
+                'status' => 1,
+            ],
+        );
+        \App\Models\AmenitiesList::updateOrCreate(
+            [
+                'amenities_name' => 'All Utilities Included',
+            ],
+            [
+                'amenities_name' => 'All Utilities Included',
+                'icon_url' => 'amenities/utilities.png',
+                'client_id' => 15465793,
+                'status' => 1,
+            ],
+        );
+    }
+}

a6b4b375677733f691eba4e0539260bede1badb2
Pooja Patel <phpatel.4518@gmail.com>
2020-11-23 12:24:46 -0500

    fix: year validation for recruiter dates, profile issues on owner profile

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -91,10 +91,10 @@ class Helper
 
     public static function get_payable_amount($payment, $is_owner = 0)
     {
-        // Total amount is rent - service for traveler
+        // Total amount is rent + service for traveler
         $amount = $payment->total_amount + $payment->service_tax;
         if ($is_owner) {
-            // Total amount is rent - service for traveler
+            // Total amount is rent - service for owner
             $amount = $payment->total_amount - $payment->service_tax;
         }
         return $amount;

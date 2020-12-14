5aabadcab8f24107f07ed54ee068a1b93b29f08a
Pooja Patel <phpatel.4518@gmail.com>
2020-12-14 06:59:33 -0500

    fix: link on delete account page redirecting to old site

--- resources/views/policies/cancellationpolicy.blade.php
+++ resources/views/policies/cancellationpolicy.blade.php
@@ -7,7 +7,7 @@
             <div id="post" class="row  post-2000 page type-page status-publish hentry">
                 <div class="col-md-12 breadcrumb_container">
                     <ol class="breadcrumb">
-                        <li><a href="http://healthcaretravels.com">Home</a></li>
+                        <li><a href="{{url('/')}}">Home</a></li>
                         <li class="active">Cancellation Policy</li>
                     </ol>
                 </div>
